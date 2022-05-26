4c94112253214ace550d1b43576faf8ac53a8d2c
ErickaAndreinaCortes <erickaandreinacortes@gmail.com>
2022-05-26 11:52:43 -0400

    cambios de renombrar funciones

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-// estas funciones son de ejemplo
+/* estas funciones son de ejemplo
 
 export const example = () => {
     return 'example';
@@ -7,8 +7,42 @@ export const example = () => {
 export const anotherExample = () => {
     return 'OMG';
 
-};
+};*/
 
+
+//Esta función reduce la data original, estamos guardando los Id de los personajes en una sola variable
+export const reduceData = (arrayCharacters) => {
+        //console.log(arrayCharacters)
+        const filterArrayForId = arrayCharacters.filter((item) => item.id ==
+            "326" || item.id == "476" || item.id == "587" ||
+            item.id == "472" || item.id == "473" ||
+            item.id == "258" || item.id == "117" ||
+            item.id == "743" || item.id == "267" ||
+            item.id == "254" || item.id == "756" ||
+            item.id == "744" || item.id == "123" ||
+            item.id == "327" || item.id == "679" ||
+            item.id == "382" || item.id == "350");
+        //console.log(arrayFiltradoPorID);
+        return filterArrayForId;
+
+    }
+    //esta función filtra a los personajes por sus casas, retornadonos el filtro realizado en la data reducida
+export const houseFilter = (filterArrayForId, houseSelection) => {
+
+        return filterArrayForId
+            .filter((houses) => houses.house == houseSelection);
+
+    }
+    /*esta función nos filtra los patronus de cada personaje en la data reducida, 
+    donde le estamos asignando dos parametros, 
+    el primero es la data y el segundo es la selección que hace el usuario.*/
+export const patronusFilter = (filterArrayForId, patronus) => {
+    return filterArrayForId
+        .filter((animal) => animal.patronus == patronus);
+}
+
+
+//filtro para que el usuario busque
 /*export const search = (query) => {
     return characters.filter(item => {
         return item.name.includes(query);
@@ -16,25 +50,7 @@ export const anotherExample = () => {
 }
 console.log(search('potter'));*/
 
-export const busqueda = (arrayCharacters) => {
-    //console.log(arrayCharacters)
-    const arrayFiltradoPorID = arrayCharacters.filter((item) => item.id ==
-        "326" || item.id == "476" || item.id == "587" ||
-        item.id == "472" || item.id == "473" ||
-        item.id == "258" || item.id == "117" ||
-        item.id == "743" || item.id == "267" ||
-        item.id == "254" || item.id == "756" ||
-        item.id == "744" || item.id == "123" ||
-        item.id == "327" || item.id == "679" ||
-        item.id == "382" || item.id == "350");
-    //console.log(arrayFiltradoPorID);
-    return arrayFiltradoPorID;
-
-}
-
-
-export const primerFiltro = (arrayFiltradoPorID, seleccionHouse) => {
-
-    return arrayFiltradoPorID.filter((elemento) => elemento.house == seleccionHouse);
-
-}
+//filtro para que el usuario ordene alfabeticamente
+/*const ordenandoDeAz = busqueda;
+ordenandoDeAz.sort((a, b) => a.name - b.name)
+console.log(ordenandoDeAz);*/
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import { busqueda } from './data.js';
-
-import { primerFiltro } from './data.js';
+import { reduceData, houseFilter, patronusFilter } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/harrypotter/data.js';
+//en esta constante estamos guardando a los personajes en total, antes de reducirla
 const characters = data.characters;
 
-
-const datosPersonajesPrincipales = busqueda(characters);
+/*esta variable guarda a la data reducida, y a su vez le aplicamos la función que nos muestra 
+a los personajes principales, esto lo logramos aplicando con el metodo map el cual 
+nos entrega un nuevo array con la información solicitada*/
+const datosPersonajesPrincipales = reduceData(characters);
 //console.log(datosPersonajesPrincipales);
 const showPersonajes = (datosPersonajesPrincipales) => {
     return datosPersonajesPrincipales.map(
@@ -42,7 +43,7 @@ document.getElementById("item1").addEventListener("click", funcionMostrarPersona
 
 document.getElementById("seleccionMenuCasas").addEventListener("change", () => {
     const guardaSeleccionCasa = document.getElementById("seleccionMenuCasas").value;
-    const mostrarPrimerFiltro = primerFiltro(datosPersonajesPrincipales, guardaSeleccionCasa);
+    const mostrarPrimerFiltro = houseFilter(datosPersonajesPrincipales, guardaSeleccionCasa);
     console.log(mostrarPrimerFiltro);
     //console.log(guardaSeleccionCasa);
     showPersonajes(mostrarPrimerFiltro);
@@ -54,3 +55,33 @@ document.getElementById("seleccionMenuCasas").addEventListener("change", () => {
 //const cambioDos = document.querySelector(".primeraMuestraPersonajes");
 //cambioDos.innerHTML = showPersonajes(mostrarPrimerFiltro);
 //console.log(mostrarPrimerFiltro);
+
+
+const filtrandoPatronus = patronusFilter(datosPersonajesPrincipales);
+console.log(filtrandoPatronus);
+const showPatronus = (datosPersonajesPrincipales) => {
+    return datosPersonajesPrincipales.map(
+            (gente) =>
+            `<div class="contenedorDePersonajes">
+            <img src="" alt=""/>
+            <p>${gente.name}</p>
+            <p>${gente.patronus}</p>
+        </div>`
+        )
+        .join("");
+
+};
+
+const cambio3 = document.querySelector(".primeraMuestraPersonajes");
+cambio3.innerHTML = showPatronus(datosPersonajesPrincipales);
+//console.log(cambio.innerHTML);
+
+function funcionMostrarPatronus() {
+    let mostrarPrincipales = document.getElementById("loQueVoyAmostrar");
+    if (mostrarPrincipales.style.display === "none") {
+        mostrarPrincipales.style.display = "block";
+    } else {
+        mostrarPrincipales.style.display = "none";
+    }
+}
+document.getElementById("item2").addEventListener("click", funcionMostrarPatronus)
