2ce351c247f2807b5cf46e9bb74687f433c7a158
ErickaAndreinaCortes <erickaandreinacortes@gmail.com>
2022-05-30 20:06:48 -0400

    cambios del sort sin terminar

--- src/data.js
+++ src/data.js
@@ -8,27 +8,25 @@ export const anotherExample = () => {
     return 'OMG';
 
 };*/
-
-
 //Esta función reduce la data original, estamos guardando los Id de los personajes en una sola variable
 export const reduceData = (arrayCharacters) => {
-        //console.log(arrayCharacters)
-        const filterArrayForId = arrayCharacters.filter((item) => item.id ==
-            "326" || item.id == "476" || item.id == "587" ||
-            item.id == "472" || item.id == "473" ||
-            item.id == "258" || item.id == "117" ||
-            item.id == "743" || item.id == "267" ||
-            item.id == "254" || item.id == "756" ||
-            item.id == "744" || item.id == "123" ||
-            item.id == "327" || item.id == "679" ||
-            item.id == "382" || item.id == "350");
-        //console.log(reduceData);
-        return filterArrayForId;
+    //console.log(arrayCharacters)
+    const filterArrayForId = arrayCharacters.filter((item) => item.id ==
+        "326" || item.id == "476" || item.id == "587" ||
+        item.id == "472" || item.id == "473" ||
+        item.id == "258" || item.id == "117" ||
+        item.id == "743" || item.id == "267" ||
+        item.id == "254" || item.id == "756" ||
+        item.id == "744" || item.id == "123" ||
+        item.id == "327" || item.id == "679" ||
+        item.id == "382" || item.id == "350");
+    //console.log(reduceData);
+    return filterArrayForId;
 
-    }
-    /*showChracters es la función contiene la información que queremos mostrar 
-    de los personajes principales, esto lo logramos aplicando con el metodo map el cual 
-    nos entrega un nuevo array con la información solicitada*/
+};
+/*showChracters es la función contiene la información que queremos mostrar 
+de los personajes principales, esto lo logramos aplicando con el metodo map el cual 
+nos entrega un nuevo array con la información solicitada*/
 export const containerInformation = (informationFirstCharacters) => {
     return informationFirstCharacters.map(
             (gente) =>
@@ -42,29 +40,25 @@ export const containerInformation = (informationFirstCharacters) => {
             </div>`
         )
         .join("");
-
 };
-
-/*Esta función oculta y muestra los personajes
-export const showAndHideCharacters = () => {
-        let showAndHideOne = document.getElementById("loQueVoyAmostrar"); //preguntar si esta linea puede ir aquí
-        if (showAndHideOne.style.display === "none") {
-            showAndHideOne.style.display = "block";
-        } else {
-            showAndHideOne.style.display = "none";
-        }
-    }*/
 //esta función filtra a los personajes por sus casas, retornadonos el filtro realizado en la data reducida
 export const houseFilter = (filterArrayForId, houseSelection) => {
-
     return filterArrayForId
         .filter((houses) => houses.house == houseSelection);
-
-}
-
-/*esta función nos filtra las varitas de cada personaje en la data reducida, 
-donde le estamos asignando dos parametros, 
-el primero es la data y el segundo es la selección que hace el usuario.*/
+};
+export const showBlood = (informationFirstCharacters) => {
+    return informationFirstCharacters.map((people) =>
+            `<div class="contenedorDePersonajes">
+                <img src="" alt=""/>
+                <p>${people.name}</p>
+                <p>${people.ancestry}</p>
+            </div>`
+        )
+        .join("")
+};
+export const ancestryFilter = (filterArrayForId, ancestrySelection) => {
+    return filterArrayForId.filter((ancestry) => ancestry.ancestry == ancestrySelection);
+};
 export const showWand = (informationFirstCharacters) => {
     return informationFirstCharacters.map(
             (people) =>
@@ -75,11 +69,11 @@ export const showWand = (informationFirstCharacters) => {
             </div>`
         )
         .join("");
-
 };
+/*esta función nos filtra las varitas de cada personaje en la data reducida, 
+donde le estamos asignando dos parametros, 
+el primero es la data y el segundo es la selección que hace el usuario.*/
 export const wandFilter = (filterArrayForId, wandSelection) => {
-    console.log(wandSelection);
-    console.log(filterArrayForId);
     const wandFilterDos = filterArrayForId.filter(wood => wood.wand);
     let personajes = [];
     wandFilterDos.forEach((Element) => {
@@ -89,15 +83,9 @@ export const wandFilter = (filterArrayForId, wandSelection) => {
         }
     })
     return (personajes);
-}
-
-
-
-
-
+};
 
 export const selecSortAz = (informationFirstCharacters) => {
-
     return informationFirstCharacters.sort((a, b) => {
         if (a.name < b.name) {
             return -1;
@@ -107,57 +95,16 @@ export const selecSortAz = (informationFirstCharacters) => {
         }
         return 0;
     });
-
 };
 
-
-/*preguntar si puede quedar sin parametro de entrada
-export const showAndHidePatronus = () => {
-    let showAndHidetwo = document.getElementById("loQueVoyAmostrar");
-    if (showAndHidetwo.style.display === "none") {
-        showAndHidetwo.style.display = "block";
-    } else {
-        showAndHidetwo.style.display = "none";
-    }
+export const selecSortZa = (informationFirstCharacters) => {
+    return informationFirstCharacters.sort((b, a) => {
+        if (b.name < a.name) {
+            return -1;
+        }
+        if (a.name > b.name) {
+            return 1;
+        }
+        return 0;
+    });
 }
-
-
-//funcion que filtra las varitas
-export const wandFilter = (filterArrayForId, wand) => {
-    return filterArrayForId
-        .filter((wood) => wood.wand == wand);
-}
-export const showWand = (informationFirstCharacters) => {
-    return informationFirstCharacters.map(
-            (people) =>
-            `<div class="contenedorDePersonajes">
-                <img src="" alt=""/>
-                <p>${people.name}</p>
-                <p>${people.wand}</p>
-            </div>`
-        )
-        .join("");
-
-};
-//preguntar si puede quedar sin parametro de entrada
-export const showAndHideWand = () => {
-    let showAndHideThree = document.getElementById("loQueVoyAmostrar");
-    if (showAndHideThree.style.display === "none") {
-        showAndHideThree.style.display = "block";
-    } else {
-        showAndHideThree.style.display = "none";
-    }
-}
-
-//filtro para que el usuario busque
-/*export const search = (query) => {
-    return characters.filter(item => {
-        return item.name.includes(query);
-    })
-}
-console.log(search('potter'));*/
-
-//filtro para que el usuario ordene alfabeticamente
-/*const ordenandoDeAz = busqueda;
-ordenandoDeAz.sort((a, b) => a.name - b.name)
-console.log(ordenandoDeAz);*/
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { reduceData, containerInformation, houseFilter, wandFilter, showWand, selecSortAz } from './data.js';
+import { reduceData, containerInformation, houseFilter, wandFilter, showWand, ancestryFilter, showBlood, selecSortAz, selecSortZa } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/harrypotter/data.js';
 //en esta constante estamos guardando a los personajes en total, antes de reducirla
@@ -6,16 +6,6 @@ const characters = data.characters;
 
 //esta variable guarda a la data reducida
 const informationFirstCharacters = reduceData(characters);
-//console.log(informationFirstCharacters);
-
-/*esta variable nos hace los cambios en el dom, el inner nos inyecta la información 
-al div vacio*/
-//const showOne = document.querySelector(".primeraMuestraPersonajes");
-//showOne.innerHTML = containerInformation(informationFirstCharacters);
-//console.log(cambio.innerHTML);
-
-/*este es el click que oculta y muestra a los personajes principales
-document.getElementById("item1").addEventListener("click", showAndHideCharacters)*/
 
 
 //este metodo nos permite tomar el id y que se escuche el cambio que realice el usuario y se 
@@ -23,73 +13,41 @@ document.getElementById("item1").addEventListener("click", showAndHideCharacters
 document.getElementById("seleccionMenuCasas").addEventListener("change", () => {
     const saveFirstHouseSelection = document.getElementById("seleccionMenuCasas").value;
     const showfirtsfilter = houseFilter(informationFirstCharacters, saveFirstHouseSelection);
-    //console.log(showfirtsfilter);
-    //console.log(guardaSeleccionCasa);
-
-    //console.log(mostrarPrimerFiltro);
     //aqui hacemos el cambio dos donde el inner inyecta al html lo que va a mostrar
-    const showtwo = document.querySelector(".primeraMuestraPersonajes");
-    showtwo.innerHTML = containerInformation(showfirtsfilter);
+    const showOne = document.querySelector(".primeraMuestraPersonajes");
+    showOne.innerHTML = containerInformation(showfirtsfilter);
 
 });
 
-/*const showTwo = document.querySelector(".primeraMuestraPersonajes");
-showTwo.innerHTML = showPatronus(informationFirstCharacters);*/
+document.getElementById("seleccionMenuLinaje").addEventListener("change", () => {
+    const saveFirstAncestrySelection = document.getElementById("seleccionMenuLinaje").value;
+    const showSecondFilter = ancestryFilter(informationFirstCharacters, saveFirstAncestrySelection);
+    const showTwo = document.querySelector(".primeraMuestraPersonajes");
+    showTwo.innerHTML = showBlood(showSecondFilter);
+});
 
 document.getElementById("seleccionMenuVaritas").addEventListener("change", () => {
     const saveFirstWandSelection = document.getElementById("seleccionMenuVaritas").value;
-    const showfirtsfilterS = wandFilter(informationFirstCharacters, saveFirstWandSelection);
+    const showThreeFilter = wandFilter(informationFirstCharacters, saveFirstWandSelection);
     //aqui hacemos el cambio dos donde el inner inyecta al html lo que va a mostrar
-    const showtwoS = document.querySelector(".primeraMuestraPersonajes");
-    showtwoS.innerHTML = showWand(showfirtsfilterS);
-    console.log(showfirtsfilterS);
+    const showthree = document.querySelector(".primeraMuestraPersonajes");
+    showthree.innerHTML = showWand(showThreeFilter);
+    console.log(saveFirstWandSelection);
 
 });
 
-const ordenando = selecSortAz(informationFirstCharacters);
-//console.log(ordenando);
-
-
-
-//esta constante guarda el filtro de los patronus
-/*const saveFilterPatronus = patronusFilter(informationFirstCharacters);
-console.log(saveFilterPatronus);
-
-
-//esto inyecta al html
-const showthree = document.querySelector(".primeraMuestraPersonajes");
-showthree.innerHTML = showPatronus(informationFirstCharacters);
-//console.log(cambio.innerHTML);
-
-
-document.getElementById("item2").addEventListener("click", showAndHidePatronus)
-
-//Lo siguiente guarda y muestra el filtro de varitas
-const saveFilterWands = wandFilter(informationFirstCharacters);
-console.log(saveFilterWands);
-
-
-//esto inyecta al html
-const showFour = document.querySelector(".primeraMuestraPersonajes");
-showFour.innerHTML = showWand(informationFirstCharacters);
-//console.log(cambio.innerHTML);
-
-
-document.getElementById("item3").addEventListener("click", showAndHideWand)
-
-
-
-/*una prueba mas
-document.getElementById("item3").addEventListener("click", () => {
-    const pruebaa = document.getElementById("item3").value;
-    const saveFilterWands = wandFilter(informationFirstCharacters);
+document.getElementById("seleccionMenuAlfabeticamente").addEventListener("change", () => {
+    const menuSort = document.getElementById("ascendente").value;
+    const organize = selecSortAz(informationFirstCharacters, menuSort);
     const showFour = document.querySelector(".primeraMuestraPersonajes");
-    showFour.innerHTML = showWand(informationFirstCharacters);
-})*/
+    showFour.innerHTML = containerInformation(organize);
 
-/*otra
-document.getElementById("item1").addEventListener("click", () => {
-    document.querySelector(".primeraMuestraPersonajes").style.display = "none";
-    document.querySelector(".primeraMuestraPersonajes").style.display = "block";
+});
 
-})*/
+document.getElementById("seleccionMenuAlfabeticamente").addEventListener("change", () => {
+    const menuSort = document.getElementById("descendente").value;
+    const organizeTwo = selecSortZa(informationFirstCharacters, menuSort);
+    console.log(organizeTwo);
+    const showFive = document.querySelector(".primeraMuestraPersonajes");
+    showFive.innerHTML = containerInformation(organizeTwo);
+});
